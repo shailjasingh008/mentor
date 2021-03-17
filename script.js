20f5b59c9d33e97a36fd7227c0c27a997ab9f4e6
shailjasingh008 <80113109+shailjasingh008@users.noreply.github.com>
2021-03-17 13:20:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+  
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+  })
+  document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+  })
+  document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o")
+  })
+  
+  document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://jsprospect.com/blog-15-beginner-javascript-projects-to-improve-your-front-end-skills")
+  })
+  document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://jsprospect.com/blog-15-beginner-javascript-projects-to-improve-your-front-end-skills")
+  })
+  document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://jsprospect.com/blog-15-beginner-javascript-projects-to-improve-your-front-end-skills")
+  })
